670fff20240d474b2441cf628dbfe956b94e5761
unknown <nhohoa.fee@gmail.com>
2021-12-22 16:20:08 +0700

    modify Html

--- problem/views.py
+++ problem/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, Cac ban. Cac ban cos khoe khong")
