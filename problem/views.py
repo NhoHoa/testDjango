902377f4959df2890a22b0b5bb49cebf62d4f761
unknown <nhohoa.fee@gmail.com>
2021-12-22 17:43:20 +0700

    Add New HTML Form

--- problem/views.py
+++ problem/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
+# Create your views here.
+class IndexView(View):
+    def get(self, request):
+        return render(request,'problem/index.html')
 
-def index(request):
-    return HttpResponse("Hello, My wife is Do thi Lan Phuong")
